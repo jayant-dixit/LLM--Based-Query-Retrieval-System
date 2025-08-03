ae53527b3fa42c1a7634f7a2be0c925570e7b20a
jayant-dixit <jayantdixit2406@gmail.com>
2025-08-03 15:12:38 +0530

    Refactor main.py to include uvicorn import and ensure proper execution of the FastAPI app

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+import os
 from router import router
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +10,11 @@ def read_root():
     return {"Hello": "World"}
 
 app.include_router(router, prefix="/api/v1")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000))  # Render sets PORT env variable
+    )
